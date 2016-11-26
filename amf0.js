a27b4b0e27ec45c144f4d9f42686050c13e1e490
arturomtm <arturomtm@gmail.com>
2016-11-26 21:17:10 +0100

    String and Long String types for AMF0 supported

--- amf0.js
+++ amf0.js
@@ -1,13 +1,32 @@
-import AMF from './amf'
-import { AMFDouble } from './amf_types'
+// import AMF from './amf'
+// import { AMFDouble } from './amf_types'
+
+var AMF = require('./amf')
+var AMFTypes = require('./amf_types')
+var AMFDouble = AMFTypes.AMFDouble
+var AMFBoolean = AMFTypes.AMFBoolean
+var AMFString = AMFTypes.AMFString
 
 class AMF0 extends AMF {
   handleNumber(value) {
-    return new AMFDouble(value)
+    const type = 0x00
+    return new AMFDouble(type, value)
   }
   handleBoolean(value) {
-    return new AMFBoolean(value)
+    const type = 0x01
+    return new AMFBoolean(type, value)
+  }
+  handleString(value) {
+    let type = 0x02
+    let bitLength = 16
+    let isLong = value.length >> bitLength
+    if (isLong) {
+      type = 0x0c
+      bitLength = 32
+    }
+    return new AMFString(type, value, { bitLength })
   }
 }
 
-export default AMF0
+//export default AMF0
+module.exports = AMF0
--- amf_types.js
+++ amf_types.js
@@ -1,15 +1,61 @@
-export class AMFDouble {
-  constructor() {}
-  encode(value) {
+class AMFType {
+  constructor(type) {
+    this.type = new Buffer([type])
+  }
+  encode(buffer) {
+    return Buffer.concat([this.type, buffer])
+  }
+}
+
+class AMFDouble extends AMFType {
+  constructor(type, value) {
+    super(type)
+    this.value = value
+  }
+  encode() {
     let buffer = new Buffer(8)
-    buffer.writeDoubleBE(value)
-    return buffer
+    buffer.writeDoubleBE(this.value)
+    return super.encode(buffer)
   }
   decode(buffer) {}
 }
 
-export class AMFBoolean {
-  constructor() {}
-  encode() {}
+exports.AMFDouble = AMFDouble
+
+class AMFBoolean extends AMFType {
+  constructor(type, value) {
+    super(type)
+    this.value = +value
+  }
+  encode() {
+    let buffer = new Buffer(1)
+    buffer.writeUInt8(this.value)
+    return super.encode(buffer)
+  }
   decode() {}
 }
+
+exports.AMFBoolean = AMFBoolean
+
+class AMFString extends AMFType {
+  constructor(type, value, options = { bitLength: 16 }) {
+    super(type)
+    this.value = value
+    this.bitLength = options.bitLength
+  }
+  encodeLength() {
+    let length = this.value.length
+    let buffer = new Buffer(this.bitLength/8)
+    this.bitLength === 16
+      ? buffer.writeUInt16BE(length)
+      : buffer.writeUInt32BE(length)
+    return buffer
+  }
+  encode() {
+    let buffer = new Buffer(this.value, 'utf8')
+    let length = this.encodeLength()
+    return super.encode(Buffer.concat([length, buffer]))
+  }
+}
+
+exports.AMFString = AMFString
