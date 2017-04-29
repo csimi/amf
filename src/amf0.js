9d8aa197e7deba608f1e4944f360c374da7e7a24
arturomtm <arturomtm@gmail.com>
2017-04-29 14:08:01 +0200

    fix namespace

--- src/amf0.js
+++ src/amf0.js
@@ -24,7 +24,7 @@ class AMF0 extends AMF {
     return new types.AMFNull(AMF0.NULL)
   }
   handleArray(value) {
-    return AMFArray.isStrict(value)
+    return types.AMFArray.isStrict(value)
       ? new types.AMFArray(AMF0.ARRAY_STRICT, value, { propertyEncoder: this })
       : new types.AMFObject(AMF0.ARRAY_ECMA, value, { propertyEncoder: this })
   }
