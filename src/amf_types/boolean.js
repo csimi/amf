5c35796cebaa9f15bb2550a622035d1cd2fdb9fd
arturomtm <arturomtm@gmail.com>
2017-07-13 22:34:44 +0200

    decode boolean type

--- src/amf_types/boolean.js
+++ src/amf_types/boolean.js
@@ -3,14 +3,20 @@ var AMFType = require('./base_type')
 class AMFBoolean extends AMFType {
   constructor(type, value) {
     super(type)
-    this.value = +value
+    this.value = value
   }
   encode() {
     let buffer = new Buffer(1)
-    buffer.writeUInt8(this.value)
+    buffer.writeUInt8(+this.value)
     return super.encode(buffer)
   }
-  decode() {}
+  decode(buffer) {
+    this.value = !!buffer.readUInt8(1)
+    return super.decode()
+  }
+  get length() {
+    return 2
+  }
 }
 
 module.exports = AMFBoolean
